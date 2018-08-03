feaa232d6120607a22f31da655a3e8e8a7c5ecf0
Kevin Crum <kevinbaileycrum@gmail.com>
2018-08-03 15:19:46 -0700

    MathMa.sh

--- input.sh
+++ input.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# This little baby takes in input
+# This little baby takes in input using read
+# notice the use of double quotes
 read NAME
 echo "Welcome $NAME"
---
+++ mathMa.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# simple maths
+# brought to you by campbells vegetable soup
+read X
+read Y
+
+echo $(( $X + $Y ))
+echo $(( $X - $Y ))
+echo $(( $X * $Y ))
+echo $(( $X / $Y ))
