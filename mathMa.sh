69b4cb66ca0b8e6642d0045b7b06b511d7ddb2ee
Kevin Crum <kevinbaileycrum@gmail.com>
2018-08-03 16:35:50 -0700

    MathMa.sh: rounds with printf

--- mathMa.sh
+++ mathMa.sh
@@ -2,6 +2,7 @@
 
 # simple maths
 # brought to you by campbells vegetable soup
+# reads two vars and does stuff
 # read X
 # read Y
 
@@ -11,7 +12,11 @@
 # echo $(( $X / $Y ))
 
 # slightly more complex math.sh
-read EXPRESSION
-echo $EXPRESSION | bc -l
+# takes an expression and pipes it to bc
+#   this also shows storing a result into a var and using printf to round
 
+read EXPRESSION
+RESULT=$( echo "scale = 4; $EXPRESSION" | bc -l ) # no space around '='
+
+printf '%.*f\n' 3 $RESULT
 
