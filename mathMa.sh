c5255f134a272fc85135d846f9e59bcd029a41d3
Kevin Crum <kevinbaileycrum@gmail.com>
2018-08-03 15:41:04 -0700

    MathMa.sh: evalutates expression with bc but no rounding

--- mathMa.sh
+++ mathMa.sh
@@ -2,10 +2,16 @@
 
 # simple maths
 # brought to you by campbells vegetable soup
-read X
-read Y
+# read X
+# read Y
 
-echo $(( $X + $Y ))
-echo $(( $X - $Y ))
-echo $(( $X * $Y ))
-echo $(( $X / $Y ))
+# echo $(( $X + $Y ))
+# echo $(( $X - $Y ))
+# echo $(( $X * $Y ))
+# echo $(( $X / $Y ))
+
+# slightly more complex math.sh
+read EXPRESSION
+echo $EXPRESSION | bc -l
+
+
